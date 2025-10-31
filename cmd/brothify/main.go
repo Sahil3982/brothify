9d9ca5d2a1a7b9e29e42aeca73a8afd323f85263
Sahil <sahil@asystechs.com>
2025-11-01 00:03:11 +0530

    Database Connection Successfully

--- cmd/brothify/main.go
+++ cmd/brothify/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
-	"github.com/brothify/internal/router"	
+
+	"github.com/brothify/internal/router"
+	"github.com/brothify/pkg/database"
 )
 
 func main() {
+	database.ConnectingDb()
 	mux := router.NewRouter()
 	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
--- pkg/database/database.go
+++ pkg/database/database.go
@@ -4,18 +4,31 @@ import (
 	"context"
 	"fmt"
 	"os"
+
 	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
-func main() {
-	connectionString := "postgres://username:Sahil@2002@localhost:5432/brothifydb"
+func ConnectingDb() {
+	godotenv.Load()
+
+	connectionString := os.Getenv("DATABASE_URL")
+
 	con, err := pgx.Connect(context.Background(), connectionString)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)		
+		os.Exit(1)
 	}
 
 	defer con.Close(context.Background())
-	
+
+	var greeting string
+	err = con.QueryRow(context.Background(), "select 'Database Connection Successfully'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Query Row Faild: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(greeting)
 
 }
