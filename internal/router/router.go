c7b7e078077dde62f5b94a622ae421f5e23812c7
sahil3982 <sahilpatel3982@gmail.com>
2025-11-16 23:30:38 +0530

    intial setup for user api

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,10 +2,11 @@ package router
 
 import (
 	"fmt"
+	"github.com/brothify/internal/middleware"
 	"net/http"
 )
 
-func NewRouter(dishHandler *DishHandler) *http.ServeMux {
+func NewRouter(dishHandler *DishHandler, userHandler *UserHandler) *http.ServeMux {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
@@ -13,6 +14,7 @@ func NewRouter(dishHandler *DishHandler) *http.ServeMux {
 	})
 	mux.HandleFunc("/v1/api/category/", dishCategory)
 	mux.Handle("/v1/api/dishes/", dishHandler)
+	mux.Handle("/v1/api/users/", middleware.AuthMiddleware(userHandler))
 
 	return mux
 }
