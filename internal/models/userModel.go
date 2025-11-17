927fb042709ee19db723a0655d8b4445034416e7
sahil3982 <sahilpatel3982@gmail.com>
2025-11-18 00:18:39 +0530

    some issue fix in login api

--- internal/models/userModel.go
+++ internal/models/userModel.go
@@ -4,7 +4,7 @@ import "time"
 
 type User struct {
 	ID        int       `json:"user_id"`
-	NAME      string    `json:"user_name"`
+	NAME      string    `json:"name"`
 	EMAIL     string    `json:"email"`
 	PASSWORD  string    `json:"password"`
 	CREATEDAT time.Time `json:"created_at"`
