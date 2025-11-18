f5e886037c3471f2512acf3a81a7355a997b9ad6
sahil3982 <sahilpatel3982@gmail.com>
2025-11-18 20:25:52 +0530

    migration setup done

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -3,7 +3,6 @@ package database
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -15,7 +14,7 @@ var DB *pgxpool.Pool
 func ConnectingDb() *pgxpool.Pool {
 	godotenv.Load()
 	connectionString := os.Getenv("DATABASE_URL")
-	log.Println("🔗 Database URL:", connectionString)
+	RunMigrations(connectionString)
 
 	pool, err := pgxpool.New(context.Background(), connectionString)
 	if err != nil {
---
+++ pkg/database/migrate.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"log"
+)
+
+func RunMigrations(DB_URL string) {
+	m, err := migrate.New("file://internal/migrations", DB_URL)
+	if err != nil {
+		log.Fatalf("Failed to create migrate instance: %v", err)
+	}
+
+	m.Up()
+	if err != nil && err != migrate.ErrNoChange {
+		log.Fatalf("Migration failed: %v", err)
+		return
+	}
+
+	log.Println("Database migrated successfully")
+
+}
