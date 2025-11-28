a5b457e40a10cafcb506ad602e442019b8a452b5
sahil3982 <sahilpatel3982@gmail.com>
2025-11-28 22:47:32 +0530

    Migration File added for db

---
+++ pkg/database/migrations.go
@@ -0,0 +1,70 @@
+package database
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func RunMigration(db *pgxpool.Pool) error {
+	queries := []string{
+		`CREATE TABLE IF NOT EXISTS users (
+   			 user_id SERIAL PRIMARY KEY,
+   			 name VARCHAR(255) NOT NULL,
+  			 email VARCHAR(255) UNIQUE NOT NULL,
+   			 password TEXT NOT NULL,
+   			 created_at TIMESTAMP DEFAULT NOW(),
+   			 updated_at TIMESTAMP DEFAULT NOW()
+		);
+		`,
+		`CREATE TABLE IF NOT EXISTS dishes (
+    		dish_id SERIAL PRIMARY KEY,
+    		dish_name VARCHAR(255) NOT NULL,
+    		cat_id INT,
+    		price NUMERIC(10,2) NOT NULL,
+    		description TEXT,
+    		dish_url TEXT,
+    		availability BOOLEAN DEFAULT TRUE,
+    		rating NUMERIC(3,2) DEFAULT 0,
+    		highlight BOOLEAN DEFAULT FALSE,
+    		created_at TIMESTAMP DEFAULT NOW(),
+    		updated_at TIMESTAMP DEFAULT NOW()
+		);
+`,
+		`CREATE TABLE IF NOT EXISTS reservations (
+			reservation_id SERIAL PRIMARY KEY,
+			user_id INT NOT NULL,
+			table_number INT NOT NULL,
+			reservation_person_name VARCHAR(255),
+			reservation_person_email VARCHAR(255),
+			reservation_person_mobile_number VARCHAR(20),
+			reservation_time VARCHAR(50),
+			reservation_date VARCHAR(50),
+			number_of_guests INT,
+			special_requests TEXT,
+			status VARCHAR(20),
+			created_at TIMESTAMP DEFAULT NOW(),
+			updated_at TIMESTAMP DEFAULT NOW()
+		);`,
+		` CREATE TABLE IF NOT EXISTS reservation_dishes (
+			id SERIAL PRIMARY KEY,
+			reservation_id INT NOT NULL REFERENCES reservation(reservation_id) ON DELETE CASCADE,
+			dish_id INT NOT NULL REFERENCES dishes(dish_id),
+			UNIQUE(reservation_id, dish_id)
+		);
+		`,
+	}
+
+	for _, q := range queries {
+		_, err := db.Exec(context.Background(), q)
+		if err != nil {
+			fmt.Println("Migration failed:", err)
+			return err
+		}		
+	}
+
+	fmt.Println("✅ Migrations applied successfully")
+	return nil
+
+}
