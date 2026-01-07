0cee8243fa9a35748492f37586e269722514d326
sahil3982 <sahilpatel3982@gmail.com>
2026-01-07 23:32:37 +0530

    migration done for category

--- pkg/database/migrations.go
+++ pkg/database/migrations.go
@@ -18,6 +18,15 @@ func RunMigration(db *pgxpool.Pool) error {
    			 updated_at TIMESTAMP DEFAULT NOW()
 		);
 		`,
+		`CREATE TABLE IF NOT EXISTS categories (
+			cat_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+			name VARCHAR(255) NOT NULL,
+			description TEXT,
+			slug VARCHAR(255) UNIQUE NOT NULL
+			created_at TIMESTAMP DEFAULT NOW(),
+   			updated_at TIMESTAMP DEFAULT NOW()
+		);
+		`,
 		`CREATE TABLE IF NOT EXISTS dishes (
 			dish_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     		dish_name VARCHAR(255) NOT NULL,
